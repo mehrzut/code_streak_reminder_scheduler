d6ee28cac07a5ceba68b9c100e8f5c5c31ad9aaa
Mehrzut <mhrzd.dev@gmail.com>
2025-03-15 12:16:19 +0330

    test

--- lib/main.dart
+++ lib/main.dart
@@ -83,7 +83,7 @@ Future<ResponseModel> setRemindersForUser(
   context.log('processing user ${user.name}');
   // Retrieve user's timezone offset
   final timezoneOffset = user.prefs.data['timezone'];
-  final notificationTime = user.prefs.data['notificationTime'] ?? '12:17:00';
+  final notificationTime = user.prefs.data['notificationTime'] ?? '12:22:00';
 
   if (timezoneOffset != null) {
     context.log('retrieved timezone offset: $timezoneOffset');
